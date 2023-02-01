980af040709123cc324d839088e67d4daf9619eb
MartynasTru <83432274+MartynasTru@users.noreply.github.com>
2023-02-01 17:30:36 +0200

    Update ticket_simple.sol

--- ticket_simple.sol
+++ ticket_simple.sol
@@ -5,8 +5,14 @@ contract Ticketing {
         uint ticketID;
         uint userID;
 }
+    struct TicketHolder {
+        string name;
+        string gender;
+        uint userID;
+}
 
 Ticket[] public tickets;
+TicketHolder[] public ticketholders;
 
 constructor() public {
 }
@@ -19,6 +25,15 @@ function createTicket(uint _ticketID, uint _userID) public {
     tickets.push(newTicket);
 }
 
+function createUser(string memory _name, string memory _gender, uint _userID) public {
+    TicketHolder memory newticketholder = TicketHolder({
+        name: _name,
+        gender: _gender,
+        userID: _userID
+    });
+    ticketholders.push(newticketholder);
+}
+
 function getUserID(uint _ticketID) public view returns (uint) {
     for (uint i = 0; i < tickets.length; i++) {
         if (tickets[i].ticketID == _ticketID) {
@@ -27,4 +42,16 @@ function getUserID(uint _ticketID) public view returns (uint) {
     }
         return 0;
     }
+
+function getTicketInfoByUserID(uint _userID) public view returns (Ticket memory) {
+    for (uint i = 0; i < tickets.length; i++) {
+      if (tickets[i].userID == _userID) {
+            return tickets[i];
+        }
+    }
 }
+
+}
+
+
+
